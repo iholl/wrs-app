e67c96e98fda3fd025e42395fafc02381d9ae2f3
Ian Michael Holl <ian.michael.holl@gmail.com>
2021-11-23 14:27:27 -0800

    Added: tailwind, layout, basic design

--- src/App.vue
+++ src/App.vue
@@ -1,137 +1,92 @@
 <template>
-  <div>
-    <h1 class="dashboard-title">Winter Raptor Surveys</h1>
-    <p class="dashboard-title">Filter the data to see the related sightings for each survey.</p>
-    <div class="indicator-container">
+  <div class="h-screen bg-gray-100">
+    <index-header />
+    <div class="pb-6 text-center">
       <div class="filter">
-        <h3>Survey ID</h3> 
-        <select v-model="category">
+        <h3 class="p-1 text-md font-thin">Survey ID</h3> 
+        <select v-model="survey_id">
           <option value="">None</option>
           <option value="10_20211110_NDOW_DEV">10_20211110_NDOW_DEV</option>
           <option value="test_20211115_mrjeffress">test_20211115_mrjeffress</option>
         </select>
       </div>
     </div>
-    <div v-for="survey in filterProductsByCategory" :key="survey.id">
-      <div class="card">
-        <header class="card-header">
-          <span class="card-header-title">
-            Survey ID: {{ survey.ndow_id }}
-          </span>
-          <p>Route ID: {{ survey.route_id }}</p>
-          <p>Survey Date: {{ survey.survey_date }}</p>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            <div class="group-content">
-              <div>
-                <p><b>OBSERVER INFORMATION</b></p>
-                <span>Name: {{ survey.leader }}</span>
-                <span> Phone: {{ survey.phone }}</span>
-                <span> Email: {{ survey.email}}</span>
-                <span> Additional Observers: {{ survey.total_observers - 1 }}</span>
-                <span v-if="survey.total_observers > 1"> Names: {{ survey.observer_names }}</span>
-              </div>
-            </div>
-            <div v-if="category" class="group-content">
-              <p><b>WEATHER INFORMATION</b></p>
-              <span>Precipitation: {{ survey.precipitation }}</span>
-              <span> Ice: {{ survey.ice }}</span>
-              <span> Fog: {{ survey.fog }}</span>
-              <span> Snow Cover: {{ survey.snow_cover }}</span>
-              <div class="grid-container">
-                <div class="grid-child purple">
-                  <p class="weather-subtitle">Start</p>
-                  <span>Sky: {{ survey.start_sky}}</span>
-                  <span> Temperature: {{ survey.start_temperature }}</span>
-                  <span> Wind: {{ survey.start_wind}}</span>
-                </div>
-                <div class="grid-child green">
-                  <p class="weather-subtitle">End</p>
-                  <span>Sky: {{ survey.end_sky}}</span>
-                  <span> Temperature: {{ survey.end_temperature }}</span>
-                  <span> Wind: {{ survey.end_wind}}</span>
-                </div>
-              </div>
-            </div>
-            <div v-if="category" class="group-content">
-              <p><b>SIGHTING INFORMATION</b></p>
-              <table>
-                <thead>
-                  <tr>
-                    <th>
-                      Species
-                    </th>
-                    <th>
-                      Count
-                    </th>
-                    <th>
-                      Activity
-                    </th>
-                    <th>
-                      Time
-                    </th>
-                    <th>
-                      Distance
-                    </th>
-                    <th>
-                      Side
-                    </th>
-                    <th>
-                      Speed
-                    </th>
-                    <th>
-                      Coordinates
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="sighting in filterProductsBySite" :key="sighting.ndow_id">
-                      <td>
-                        {{ sighting.species }}
-                      </td>
-                      <td>
-                        {{ sighting.number_species }}
-                      </td>
-                      <td>
-                        {{ sighting.activity }}
-                      </td>
-                      <td>
-                        {{ sighting.sight_time }}
-                      </td>
-                      <td>
-                        {{ sighting.distance }}
-                      </td>
-                      <td>
-                        {{ sighting.road_side }}
-                      </td>
-                      <td>
-                        {{ sighting.obs_mph }}
-                      </td>
-                      <td>
-                        {{ sighting.x }}, {{ sighting.y }}
-                      </td>
-                    </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+    <div v-for="survey in filterSurveys" :key="survey.id" class="px-3">
+      <div class="py-3">
+        <div class="bg-white px-4 py-2 border-b border-gray-200">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            {{ survey.route_id }} - {{ survey.survey_date }}
+          </h3>
         </div>
+        <div class="bg-white overflow-hidden shadow-md">
+          <dl class="divide-y divide-gray-200">
+            <div class="px-4 py-1">
+              <dt class="text-sm font-semibold text-gray-500">
+                Crew Leader Information
+              </dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 capitalize">
+                {{ survey.leader }} ({{ survey.affiliation }})
+              </dd>
+              <dd class="mt-1 text-sm text-gray-900 capitalize">
+                <div class="py-1">
+                  <span class="pr-1 text-sm font-medium text-gray-500">Email:</span>
+                  <span>{{ survey.email }}</span>
+                </div>
+              </dd>
+              <dt class="mt-1 text-sm font-semibold text-gray-500">
+                Weather Information
+              </dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 capitalize">
+                <div class="py-1">
+                  <span class="pr-1 text-sm font-medium text-gray-500">Precipitation:</span>
+                  <span>{{ survey.precipitation }}</span>
+                  <span class="px-1 text-sm font-medium text-gray-500">Ice:</span>
+                  <span>{{ survey.ice }}</span>
+                  <span class="px-1 text-sm font-medium text-gray-500">Fog:</span>
+                  <span>{{ survey.fog }}</span>
+                  <span class="px-1 text-sm font-medium text-gray-500">Snow Cover:</span>
+                  <span>{{ survey.snow_cover }}</span>
+                </div>
+              </dd>
+            </div>
+          </dl>
+        </div>
+        <div v-if="survey_id" class="bg-white overflow-hidden shadow-md">
+          <div class="bg-white px-4 py-2 border-b border-gray-200">
+            <h4 class="text-md leading-6 font-thin text-gray-900">
+              Sightings
+            </h4>
+          </div>
+          <ul role="list" class="pb-3 divide-y divide-gray-200">
+            <li v-for="sighting in filterSightings" :key="sighting.id" class="px-4 py-2">
+              <h5 class="text-md text-gray-700">{{ sighting.sight_time }}</h5>
+              <p class="px-1 text-sm text-gray-700">{{ sighting.species }} ({{ sighting.number_species }}) {{ sighting.activity }}, {{ sighting.road_side }}, {{ sighting.distance }}(m) ({{ sighting.x }}, {{ sighting.y }})</p>
+            </li>
+          </ul>
+        </div>
+        <!-- <div class="bg-white px-4 py-5 border-b border-gray-200">
+          {{ sightings }}
+        </div> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue"
+import IndexHeader from "@/components/index-header.vue"
+// import SurveyCard from "@/components/index-header.vue"
 
 import { getSurveys, getSightings } from "@/services/wrs-services.js"
 
 export default {
   name: "App",
+  components: {
+    IndexHeader,
+    // SurveyCard
+  },
   data () {
     return {
-      category: "",
+      survey_id: "",
       surveys: null,
       sightings:  null,
     }
@@ -146,11 +101,11 @@ export default {
     })
   },
   computed: {
-    filterProductsByCategory: function(){
-        return this.surveys.filter(survey => !survey.ndow_id.indexOf(this.category))
+    filterSurveys: function(){
+        return this.surveys.filter(survey => !survey.ndow_id.indexOf(this.survey_id))
     },
-    filterProductsBySite: function(){
-        return this.sightings.filter(sighting => !sighting.ndow_id.indexOf(this.category))
+    filterSightings: function(){
+        return this.sightings.filter(sighting => !sighting.ndow_id.indexOf(this.survey_id))
     }
   }
 };
@@ -162,71 +117,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
-
-.filter {
-  padding: 12px 0 12px 32px;
-}
-
-.weather-subtitle {
-  font-weight: 100;
-  margin: 12px 0 8px 0;
-}
-
-.dashboard-title {
-  text-align: center;
-}
-
-.card-header-title {
-  font-size: 20px;
-  font-weight: 100;
-}
-
-.grid-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 20px;
-}
-
-.group-content {
- border: 1px solid grey;
- border-radius: 5px;
- padding: 0 12px 12px 12px;
- margin-top: 8px;
-}
-
-.indicator-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 20px;
-}
-
-.indicator-card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  border-radius: 5px;
-  margin: 24px;
-  padding: 12px;
-  text-align: center;
-}
-
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  border-radius: 5px;
-  margin: 24px;
-  padding: 12px;
-}
-
-/* On mouse-over, add a deeper shadow */
-.card:hover {
-  scale: 1.03;
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
-
-/* Add some padding inside the card container */
-/* .container {
-  padding: 2px;
-} */
 </style>
---
+++ src/components/index-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pt-9 pb-3 text-center">
+    <div class="text-3xl font-semibold">
+      Winter Raptor Surveys
+    </div>
+    <div class="text-sm text-gray-400">
+      Filter the data to see the related sightings for each survey
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index-header"
+}
+</script>
