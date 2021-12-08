b27b65a6ed82a045a181bb021ed074ad6a085338
iholl <ian.michael.holl@gmail.com>
2021-12-08 15:53:25 -0800

    Adjusted: background color

--- src/App.vue
+++ src/App.vue
@@ -133,6 +133,7 @@ export default {
       surveys: null,
       sightings: null,
       geojson: null,
+      formatOptions: { format: "MM-dd-yyyy", type: "date" }
     };
   },
   async created () {
@@ -169,5 +170,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  height: 100vh;
 }
 </style>
